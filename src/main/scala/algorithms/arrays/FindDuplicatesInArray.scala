35969deb72c8e309713fe68af35887d85caa0d1c
Aamir.Fayaz@zettalabs.com <Aamir.Fayaz@zettalabs.com>
2019-11-02 20:44:36 +0530

    array duplicate element mehtod 1

---
+++ src/main/scala/algorithms/arrays/FindDuplicatesInArray.scala
@@ -0,0 +1,34 @@
+package algorithms.arrays
+
+/**
+  * References:
+  * link1: https://www.geeksforgeeks.org/find-the-two-repeating-elements-in-a-given-array/
+  **/
+
+/**
+  * array of elements n + 2, all elements in array are from range 1 to n.
+  * And all elements occur once except two numbers which occur twice. Find the two repeating numbers.
+  */
+
+
+/**
+  * Method 1:
+  * Time Complexity: O(n)
+  * Space Complexity: O(n)
+  **/
+object Method1 extends App {
+
+  //array of elements n + 2, all elements in array are from range 1 to n.
+  //And all elements occur once except two numbers which occur twice. Find the two repeating numbers.
+  val arr = Array(4, 2, 4, 5, 2, 3, 1)
+
+  logRepeatingElements(arr)
+  def logRepeatingElements(arr: Array[Int]): Unit = {
+
+    for {
+      i <- arr.indices
+      j <- i + 1 until arr.length
+    } if (arr(i) == arr(j)) println(arr(i))
+  }
+
+}
