192a1d0dd07de874ef9f63e1ef20298fb21d5fa3
afz <dropmeataamir@gmail.com>
2020-04-03 12:43:35 +0530

    P02 of scala 99

--- src/main/scala/algorithms/scala99/P02.scala
+++ src/main/scala/algorithms/scala99/P02.scala
@@ -1,7 +1,46 @@
 package algorithms.scala99
 
+import scala.annotation.tailrec
+
+// P02 Find the last but one element of a list.
+
 object P02 extends App {
-  val l: PartialFunction[Int , Int] = List(1, 2, 3)
+
+  val l: PartialFunction[Int, Int] = List(1, 2, 3)
   val lifted: Int => Option[Int] = l.lift
-  println(l)
+  val x: Option[Int] = lifted(0)
+
+  //tail recursion
+  @tailrec
+  def f0[T](list: List[T]): T = list match {
+    case Nil => throw new NoSuchElementException("List is empty")
+    //case x :: Nil => throw new NoSuchElementException("singleton list")
+    case List(_)    => throw new NoSuchElementException("singleton list")
+    case List(x, _) => x
+    //case x :: _ :: Nil => x
+    case _ :: tail => f0(tail)
+  }
+
+  //function application or chained calls
+  def f1[T](list: List[T]): T = list.init.last
+
+  //with error check
+  def f2[T](list: List[T]): T = list.init.lastOption.getOrElse(throw new NoSuchElementException("---calling last of empty init----"))
+
+  //using index
+  def f3[T](list: List[T]): T = list.reverse(1)
+
+  //with partial function lifting
+  def f4[T](list: List[T]): T = list.reverse.lift(1).getOrElse(throw new NoSuchElementException("empty list or singleton list"))
+
+  //using composition of function
+  def f5[T]: List[T] => T = ((_: List[T]).last).compose((_: List[T]).init)
+
+  println(f0(List(1, 2, 3, 4)))
+  println(f1(List(1, 2, 3, 4)))
+  println(f2(List(1, 2, 3, 4)))
+  println(f3(List(1, 2, 3, 4)))
+  println(f4(List(1, 2, 3, 4)))
+  println(f5(List(1, 2, 3, 4)))
+
 }
