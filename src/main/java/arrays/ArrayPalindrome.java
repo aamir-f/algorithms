0ffb23a7d52e67f6ad1b8649ec8129a45d890e04
afz <dropmeataamir@gmail.com>
2020-04-26 13:17:29 +0530

    sum of digits of a number

--- src/main/java/arrays/ArrayPalindrome.java
+++ src/main/java/arrays/ArrayPalindrome.java
@@ -9,10 +9,25 @@ public class ArrayPalindrome {
         return true;
     }
 
+    public static int reverseDigits(int num, int rev) {
+        if (num == 0) return rev;
+        else return reverseDigits(num / 10, ((10 * rev) + num % 10));
+    }
+
+    public static boolean arrayOfPalindromesCheck(int[] arr) {
+        for (int item: arr) {
+
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 8, 1};
         boolean check = isPalindrome(arr);
         if (check) System.out.println("Palindrome");
         else System.out.println("Not Palindrome");
+
+        System.out.println("Now...arr of palindromes check");
+        int[] arr1 = {111, 222, 333, 444, 555};
     }
 }
